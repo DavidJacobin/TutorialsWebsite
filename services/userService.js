a85a79cb0fe4efc895957729170ced3ee2fb5353
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-25 10:38:09 +0200

    creating register with bcrypt and jwt

---
+++ services/userService.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'tj54u389tyy8t'
+
+
+async function register(username, password){
+    const existing = User.findOne({username}).collation({locale: 'en', strength: 2});
+
+    if (existing){
+        throw new Error('Username is taken!')
+    };
+
+    const hashedPassword = bcrypt.hash(password,10);
+
+    const user = await User.create({
+        username, 
+        hashedPassword
+    });
+
+    const token = createSession(user);
+
+    return token;
+};
+
+function createSession({_id, username}){
+    const payload = {
+        _id,
+        username
+    }
+
+    const token = jwt.sign(payload,JWT_SECRET);
+
+    return token;
+};
+
+
+module.exports = {
+    register,
+    login
+};
