13a27e157656c19b4e83d87d09078748d704c46b
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-28 12:25:55 +0200

    adding express-validator for validating register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const { register, login } = require('../services/userService');
 const { errorParser } = require('../util/errorParser');
 
@@ -7,40 +8,47 @@ authController.get('/register', (req, res) => {
     res.render('register');
 });
 
-authController.post('/register', async (req, res) =>{
-    try {
+authController.post('/register',
+    body('username')
+        .isLength({ min: 5 }).withMessage("Username must be at least 5 characters long!")
+        .isAlphanumeric().withMessage('Username must contain only letters and numbers!'),
+    body('password')
+        .isAlphanumeric().withMessage('Password must contain only letters and numbers!')
+        .isLength({ min: 5 }).withMessage("Password must be at least 5 characters long!")
+    , async (req, res) => {
+        try {
+            const { errors } = validationResult(req);
+            if (errors.length > 0) {
+                throw errors
+            };
 
-        if(req.body.username == "" || req.body.password == ""){
-            throw new Error('All feilds are required!')
+            if (req.body.password != req.body.repassword) {
+                throw new Error('Passwords don\'t match!')
+            };
+
+            const token = await register(req.body.username, req.body.password);
+
+            res.cookie('token', token);
+
+            res.redirect('/');
+
+        } catch (err) {
+            const errors = errorParser(err)
+
+            res.render('register', {
+                errors,
+                body: { username: req.body.username }
+            });
+
         };
 
-        if(req.body.password != req.body.repassword){
-            throw new Error('Passwords don\'t match!')
-        };
-
-        const token = await register(req.body.username, req.body.password);
-    
-        res.cookie('token', token);
-    
-        res.redirect('/');
-        
-    } catch (err) {
-        const errors = errorParser(err)
-
-        res.render('register',{
-            errors,
-            body:{username: req.body.username}
-        });
-        
-    };
-
-});
+    });
 
 authController.get('/login', (req, res) => {
     res.render('login');
 });
 
-authController.post('/login',async (req, res) =>{
+authController.post('/login', async (req, res) => {
     try {
         const token = await login(req.body.username, req.body.password);
 
@@ -48,14 +56,14 @@ authController.post('/login',async (req, res) =>{
         res.redirect('/');
     } catch (err) {
         const errors = errorParser(err);
-        res.render('login',{
+        res.render('login', {
             errors,
-            body: {username : req.body.username}
+            body: { username: req.body.username }
         })
     }
 });
 
-authController.get('/logout', (req,res) =>{
+authController.get('/logout', (req, res) => {
     res.clearCookie('token');
     res.redirect('/');
 });
